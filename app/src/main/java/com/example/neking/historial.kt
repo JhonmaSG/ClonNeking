b6aea6f4ca95c950f3b6f5d50c3b6feda20d7a94
Furotsuki <juanpablodazab@gmail.com>
2024-09-05 21:04:25 -0500

    Creacion de todas las vista faltantes

--- app/src/main/java/com/example/neking/historial.kt
+++ app/src/main/java/com/example/neking/historial.kt
@@ -3,14 +3,13 @@ package com.example.neking
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class historial : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.historial)
+        setContentView(R.layout.detalle_historico_r)
 
-        }
     }
+}
