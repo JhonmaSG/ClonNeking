2b5982c9b21a8a892cbe64e51422e5b395cabab5
Furotsuki <juanpablodazab@gmail.com>
2024-09-14 20:50:00 -0500

    Agregamiento de logica base historial

--- app/src/main/java/com/example/neking/historial.kt
+++ app/src/main/java/com/example/neking/historial.kt
@@ -6,6 +6,8 @@ import android.widget.Button
 import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 
 class historial : AppCompatActivity() {
@@ -18,6 +20,17 @@ class historial : AppCompatActivity() {
             val intent = Intent(this, detalle_historico_c::class.java)
             startActivity(intent)
         }**/
+        var rv_comprobantes: RecyclerView = findViewById(R.id.recycle_v_e)
+        rv_comprobantes.layoutManager = LinearLayoutManager(this)
+
+        var listaComprobantes = ArrayList<lista_comprobantes>()
+        listaComprobantes.add(lista_comprobantes("Chao","De","-$50.000,00"))
+        listaComprobantes.add(lista_comprobantes("Pepe","De","-$50.000,00"))
+        listaComprobantes.add(lista_comprobantes("Antonio","De","-$50.000,00"))
+        listaComprobantes.add(lista_comprobantes("Jose","De","-$50.000,00"))
+
+        rv_comprobantes.adapter = conexion_lista(listaComprobantes)
+
 
     }
 }
