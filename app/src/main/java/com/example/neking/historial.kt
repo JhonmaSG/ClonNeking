4a096af17d08446b256e0e595e1206baba0bf962
Furotsuki <juanpablodazab@gmail.com>
2024-09-15 12:36:12 -0500

    Agregamiento de logica de ruta,adecuacion de diseño de comprobantes

--- app/src/main/java/com/example/neking/historial.kt
+++ app/src/main/java/com/example/neking/historial.kt
@@ -2,8 +2,7 @@ package com.example.neking
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -11,15 +10,19 @@ import androidx.recyclerview.widget.RecyclerView
 
 
 class historial : AppCompatActivity() {
+    private lateinit var onClickListener: (Int) -> Unit
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.historial)
-        /**val btn_regresar : Button = findViewById(R.id.chao)
+        /**
+        val btn_regresar : Button = findViewById(R.id.chao)
         btn_regresar.setOnClickListener {
             val intent = Intent(this, detalle_historico_c::class.java)
             startActivity(intent)
         }**/
+
+
         var rv_comprobantes: RecyclerView = findViewById(R.id.recycle_v_e)
         rv_comprobantes.layoutManager = LinearLayoutManager(this)
 
@@ -30,7 +33,6 @@ class historial : AppCompatActivity() {
         listaComprobantes.add(lista_comprobantes("Jose","De","-$50.000,00"))
 
         rv_comprobantes.adapter = conexion_lista(listaComprobantes)
-
 
     }
 }
